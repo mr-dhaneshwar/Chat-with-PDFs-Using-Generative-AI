c4b7aada0478d2a7c98f8edb1317e0e10483f379
mr-dhaneshwar <prasaddhaneshwar09@gmail.com>
2024-08-01 11:09:21 +0530

    same change by previous

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def get_text_chunks(text):
 
 
 def get_vector_store(text_chunks):
-    dltfaiss()
+    # dltfaiss()
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
